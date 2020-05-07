f3cfd7ff91c20bd817f9ab5afd18b75576de0fa9
a3v <a3vinogradov@gmail.com>
2020-05-07 07:52:31 +0300

    добавлены исходники по главе 32

---
+++ 314425 ch05 code/Generics/GenericsSamples/GenericMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Wrox.ProCSharp.Generics
+{
+  class Program
+  {
+    static void Main()
+    {
+      var accounts = new List<Account>()
+      {
+        new Account("Christian", 1500),
+        new Account("Stephanie", 2200),
+        new Account("Angela", 1800),
+        new Account("Matthias", 2400)
+      };
+
+      decimal amount = Algorithm.AccumulateSimple(accounts);
+            Console.WriteLine(amount);
+
+      amount = Algorithm.Accumulate(accounts);
+            Console.WriteLine(amount);
+
+            amount = Algorithm.Accumulate<Account, decimal>(accounts, (item, sum) => sum += item.Balance);
+            Console.WriteLine(amount);
+            Console.ReadLine();
+    }
+  }
+}
---
+++ 314425 ch32 code/01_ExecutingCommands/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _01_ExecutingCommands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteNonQuery();
+            ExecuteReader();
+            ExecuteScalar();
+            ExecuteXmlReader();
+        }
+
+        static void ExecuteNonQuery()
+        {
+            string select = "UPDATE Customers " +
+                            "SET ContactName = 'Bob' " +
+                            "WHERE ContactName = 'Bill'";
+            SqlConnection conn = new SqlConnection(GetDatabaseConnection());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(select, conn);
+            int rowsReturned = cmd.ExecuteNonQuery();
+            Console.WriteLine("{0} rows returned.", rowsReturned);
+            conn.Close();
+        }
+
+        static void ExecuteReader()
+        {
+            string select = "SELECT ContactName,CompanyName FROM Customers";
+            SqlConnection conn = new SqlConnection(GetDatabaseConnection());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(select, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("Contact: {0,-20} Company: {1}",
+                                   reader[0], reader[1]);
+            }
+        }
+
+        static void ExecuteScalar()
+        {
+            string select = "SELECT COUNT(*) FROM Customers";
+            SqlConnection conn = new SqlConnection(GetDatabaseConnection());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(select, conn);
+            object o = cmd.ExecuteScalar();
+            Console.WriteLine(o);
+        }
+
+        static void ExecuteXmlReader()
+        {
+            string select = "SELECT ContactName,CompanyName " +
+                            "FROM Customers FOR XML AUTO";
+            SqlConnection conn = new SqlConnection(GetDatabaseConnection());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(select, conn);
+            XmlReader xr = cmd.ExecuteXmlReader();
+            xr.Read();
+            string data;
+            do
+            {
+                data = xr.ReadOuterXml();
+                if (!string.IsNullOrEmpty(data))
+                    Console.WriteLine(data);
+            } while (!string.IsNullOrEmpty(data));
+            conn.Close();
+
+        }
+
+        static string GetDatabaseConnection()
+        {
+            return "server=(local);" +
+                "integrated security=SSPI;" +
+                "database=Northwind";
+        }
+    }
+}
