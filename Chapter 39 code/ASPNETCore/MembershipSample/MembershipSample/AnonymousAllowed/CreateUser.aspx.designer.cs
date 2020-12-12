bcaa61d7269971de994eb0467eb24eec71abe40d
Vinogradov Alexander <a3vinogradov@gmail.com>
2020-12-12 22:01:12 +0300

    Chapter 39 приложение StateSimple

--- Chapter 39 code/ASPNETCore/MembershipSample/MembershipSample/AnonymousAllowed/CreateUser.aspx.designer.cs
+++ Chapter 39 code/ASPNETCore/MembershipSample/MembershipSample/AnonymousAllowed/CreateUser.aspx.designer.cs
@@ -7,11 +7,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MembershipSample {
-    
-    
-    public partial class CreateUser {
-        
+namespace MembershipSample
+{
+
+
+    public partial class CreateUser
+    {
+
         /// <summary>
         /// form1 control.
         /// </summary>
@@ -20,7 +22,7 @@ namespace MembershipSample {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
+
         /// <summary>
         /// Button1 control.
         /// </summary>
